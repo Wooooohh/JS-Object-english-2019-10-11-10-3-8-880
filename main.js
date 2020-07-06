b54147e146d54b5eca65017a6334e7d80356475f
Wooooohh <42633990+Wooooohh@users.noreply.github.com>
2020-07-06 18:10:52 +0800

    Create main.js
    write js code

---
+++ main.js
@@ -0,0 +1,12 @@
+1.
+var user = {null};
+user.name = "John"
+user.surname = "Mike"
+user.name = "Peter"
+
+2.
+var values = Object.values(fruit)
+var sum=0;
+for(var i = 0; i < values.length; i++){
+  sum +=values[i];
+}
